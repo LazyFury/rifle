981f4ef983bdce30836c404019c38d595c198a60
robot <suke971219@gmail.com>
2024-03-15 15:13:06 +0800

    backup sql

--- backup.db.sh
+++ backup.db.sh
@@ -11,61 +11,21 @@ password=$PASSWORD
 
 backup_dir="./database/backups/"
 
-function backup_table () {
-    local table_name=$1
-    local backup_file="$backup_dir$table_name.sql"
-    echo "Backing up $table_name to $backup_file"
-    echo "-h $host -P $port -u $user -p$password $database $table_name > $backup_file"
-    # mysqldump -h $host -P $port -u $user -p$password $database $table_name --no-tablespaces > $backup_file
-    # only insert
-    mysqldump -h $host -P $port -u $user -p$password $database $table_name --no-create-info --no-tablespaces > $backup_file
-}
-
-function import_table () {
-    local table_name=$1
-    local backup_file="$backup_dir$table_name.sql"
-    echo "Importing $table_name from $backup_file"
-    # echo "-h $host -P $port -u $user -p$password $database $table_name < $backup_file"
-    mysql -h $host -P $port -u $user -p$password $database $table_name < $backup_file
-}
-
-# $1 is import 
-if [ "$1" == "import" ]; then
-    import_table "permissions"
-    import_table "users"
-    import_table "roles"
-    import_table "role_has_permissions"
-    import_table "menus"
-    import_table "dicts"
-    import_table "dict_groups"
-    import_table "api_manages"
-    exit 0    
-fi
-
-# $1 is backup
-if [ "$1" == "backup" ]; then
-    backup_table "permissions"
-    backup_table "users"
-    backup_table "roles"
-    backup_table "role_has_permissions"
-    backup_table "menus"
-    backup_table "dicts"
-    backup_table "dict_groups"
-    backup_table "api_manages"
-    exit 0
-fi
-
-    # backup_table "permissions"
-    # backup_table "users"
-    # backup_table "roles"
-    # backup_table "role_has_permissions"
-    # backup_table "menus"
-    # backup_table "dicts"
-    # backup_table "dict_groups"
-    # backup_table "api_manages"
-    # exit 0
-
-echo "Usage: backup.db.sh [import|backup]"
-exit 1
 
 
+# get all args from command line
+excludes=(
+    "telescope_entries"
+    "teloscope_entry_tags"
+    "teloscope_tags"
+)
+exclude_str=""
+for exclude in "${excludes[@]}"; do
+    exclude_str+=" --ignore-table=${database}.${exclude}"
+done
+
+echo "Excludes: ${exclude_str}"
+
+
+echo "-h $host -P $port -u $user -p$password $exclude_str  $database> $backup_dir$database-$(date +%Y-%m-%d-%H-%M-%S).sql"
+mysqldump -h $host -P $port -u $user -p$password $exclude_str  $database> $backup_dir$database-$(date +%Y-%m-%d-%H-%M-%S).sql
