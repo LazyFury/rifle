4b8539269e970d18c4f13eeaff9b4a54ec38e22c
robot <suke971219@gmail.com>
2024-03-15 13:51:13 +0800

    backup

--- backup.db.sh
+++ backup.db.sh
@@ -4,8 +4,11 @@
 host="127.0.0.1"
 port="3306"
 user="rifle"
-password="rifle"
 database="rifle"
+
+read -s -p "Enter MySQL password: " PASSWORD
+password=$PASSWORD
+
 backup_dir="./database/backups/"
 
 function backup_table () {
@@ -18,13 +21,51 @@ function backup_table () {
     mysqldump -h $host -P $port -u $user -p$password $database $table_name --no-create-info --no-tablespaces > $backup_file
 }
 
-# Backup the database
-backup_table "permissions"
-backup_table "users"
-backup_table "roles"
-backup_table "role_has_permissions"
-backup_table "menus"
-backup_table "dicts"
-backup_table "dict_groups"
-backup_table "api_manages"
+function import_table () {
+    local table_name=$1
+    local backup_file="$backup_dir$table_name.sql"
+    echo "Importing $table_name from $backup_file"
+    # echo "-h $host -P $port -u $user -p$password $database $table_name < $backup_file"
+    mysql -h $host -P $port -u $user -p$password $database $table_name < $backup_file
+}
+
+# $1 is import 
+if [ "$1" == "import" ]; then
+    import_table "permissions"
+    import_table "users"
+    import_table "roles"
+    import_table "role_has_permissions"
+    import_table "menus"
+    import_table "dicts"
+    import_table "dict_groups"
+    import_table "api_manages"
+    exit 0    
+fi
+
+# $1 is backup
+if [ "$1" == "backup" ]; then
+    backup_table "permissions"
+    backup_table "users"
+    backup_table "roles"
+    backup_table "role_has_permissions"
+    backup_table "menus"
+    backup_table "dicts"
+    backup_table "dict_groups"
+    backup_table "api_manages"
+    exit 0
+fi
+
+    # backup_table "permissions"
+    # backup_table "users"
+    # backup_table "roles"
+    # backup_table "role_has_permissions"
+    # backup_table "menus"
+    # backup_table "dicts"
+    # backup_table "dict_groups"
+    # backup_table "api_manages"
+    # exit 0
+
+echo "Usage: backup.db.sh [import|backup]"
+exit 1
+
 
